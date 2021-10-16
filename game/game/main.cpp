96c7aee04792ea7e4fcb1a3d1960b35a1228097b
Dil-Mahmud-Khan <70931637+Dil-Mahmud-Khan@users.noreply.github.com>
2021-10-16 12:00:31 +0600

    Add files via upload

---
+++ game/game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Dil on 5/6/20.
+//  Copyright © 2020 Dil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
